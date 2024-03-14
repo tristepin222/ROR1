af14bed0119fc5b14b9ae11861562d43729affee
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-14 08:12:56 +0100

    feat: added devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,19 @@ Rails.application.routes.draw do
   resources :person_learn_subjects
   resources :person_teach_subjects
   resources :person_evaluate_exams
-  resources :person_have_exams
   resources :person_belongs_to_classrooms
   resources :classrooms
   resources :exams
   resources :subjects
-  resources :people
-  resources :localities
+  resources :person_have_exams, path: 'grades', only: [:create, :update, :destroy, :edit, :new, :show]
+
+
+  resources :people do
+    resources :person_have_exams, path: 'grades'
+  end
+
+  devise_for :people, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -17,5 +23,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "people#index"
 end
