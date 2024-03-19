69e4e7526288989c3c934be1eb155acb6c951102
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-19 11:54:41 +0100

    rework: auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,33 +2,35 @@ class ApplicationController < ActionController::Base
 
   def authorize_teacher
     unless current_person&.teacher?
-      flash[:alert ] = "You are not authorized to access this page."
-      redirect_back fallback_location: root_path
+      route_back
     end
   end
 
   def authorize_dean
     unless current_person&.dean?
-      flash[:alert ] = "You are not authorized to access this page."
-      redirect_back fallback_location: root_path
+      route_back
     end
   end
 
   def authorize_dean_or_teacher
     unless current_person&.dean? || current_person&.teacher?
-      flash[:alert] = "You are not authorized to access this page."
-      redirect_back fallback_location: root_path
+      route_back
     end
   end
 
   def authorize_access
+    authorize_dean_or_teacher
     if current_person&.dean?
       # Deans can access all actions
       return
     elsif current_person&.teacher? && params[:action] != "index"
       # Teachers can only access index action
-      flash[:alert] = "You are not authorized to access this page."
-      redirect_back fallback_location: root_path
+      route_back
     end
+  end
+
+  def route_back
+    flash[:alert] = "You are not authorized to access this page."
+    redirect_back fallback_location: root_path
   end
 end
