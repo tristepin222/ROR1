d04166f54dca05b05ed30ce1e9556f3c7507a717
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-19 11:54:53 +0100

    feat: switch to lessons

--- app/models/person.rb
+++ app/models/person.rb
@@ -8,15 +8,13 @@ class Person < ApplicationRecord
   has_many :person_belongs_to_classrooms, autosave: true
   has_many :person_have_exams
   has_many :person_evaluate_exams
-  has_many :person_learn_subjects
-  has_many :person_teach_subjects
+  has_many :lessons
 
   has_many :classrooms, through: :person_belongs_to_classrooms, autosave: true
-  has_many :subjects, through: :person_learn_subjects
-  has_many :subjects, through: :person_teach_subjects
+  has_many :subjects, through: :lessons
   has_many :exams, through: :person_have_exams
   has_many :exams, through: :person_evaluate_exams
-  
+
   accepts_nested_attributes_for :locality
   accepts_nested_attributes_for :classrooms
 
