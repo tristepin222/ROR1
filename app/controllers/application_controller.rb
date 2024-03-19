475a8fccb36374ae45c25030e3d0bd7e3935b906
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-19 08:11:48 +0100

    feat: dean or teacher auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  def authorize_teacher
+    unless current_person&.teacher?
+      flash[:alert ] = "You are not authorized to access this page."
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  def authorize_dean
+    unless current_person&.dean?
+      flash[:alert ] = "You are not authorized to access this page."
+      redirect_back fallback_location: root_path
+    end
+  end
 end
