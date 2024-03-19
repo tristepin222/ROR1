490300f0f700a296886e92944cf13c1ef5e6d42f
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-19 08:42:07 +0100

    feat: changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -17,7 +17,7 @@ Rails.application.routes.draw do
     resources :person_have_exams, path: 'grades'
   end
 
-  devise_for :people, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+  devise_for :people, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret'}
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
