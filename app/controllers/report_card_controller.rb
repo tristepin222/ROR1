e7d11627f5146882a044dbb55bf0191a3260f3a9
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-21 16:46:56 +0100

    refactor: report card

--- app/controllers/report_card_controller.rb
+++ app/controllers/report_card_controller.rb
@@ -1,6 +1,5 @@
 class ReportCardController < ApplicationController
-  before_action :set_classroom, only: %i[ show edit update destroy ]
-  before_action :authorize_access, only: [:new, :create, :destroy, :update, :index]
+  before_action :authorize_access, only: [:generate]
   def generate
     @person_have_exams = PersonHaveExam.all
   end
