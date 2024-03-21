6def012a331d81482fa9990209b323109c290026
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-21 10:26:56 +0100

    removed: is_teacher

--- db/migrate/20240201163642_create_people.rb
+++ db/migrate/20240201163642_create_people.rb
@@ -8,7 +8,6 @@ class CreatePeople < ActiveRecord::Migration[7.1]
       t.string :address
       t.string :phone_number
       t.string :email
-      t.boolean :is_teacher
       t.references :locality, null: true, foreign_key: true
 
       t.timestamps
