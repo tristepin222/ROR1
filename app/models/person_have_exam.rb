ec6928f1aca0aa81adfd0869f98e3eb54dcc16ca
tristepin222 <71553096+tristepin222@users.noreply.github.com>
2024-03-22 13:36:48 +0100

    feat: validation on grade

--- app/models/person_have_exam.rb
+++ app/models/person_have_exam.rb
@@ -2,5 +2,5 @@ class PersonHaveExam < ApplicationRecord
   belongs_to :person
   belongs_to :exam
   accepts_nested_attributes_for :person
-  validates :grade, presence: true
+  validates :grade, numericality: { in: 1..6 }
 end
